71020229e015974825a6fff06403d429d367b8ab
han253 <han315730@gmail.com>
2021-08-26 17:43:24 -0500

    add js and styles

---
+++ js/contacto.js
@@ -0,0 +1,20 @@
+console.log("Hola mundo");
+
+var a = "este es un mensaje";
+
+var array_test = [1,2,3,"hola",5,6,7.5,8,"este es otro"];
+
+for(index in array_test){    
+    console.log(index);
+}
+
+for(element_a of array_test){    
+    console.log(element_a);
+}
+
+if(true || false){
+    console.log("genial");
+} else {
+    console.log("no tan genial");
+}
+
